5c3eb677a2e0e5d697fc4362da0047d475ddd731
NANDINI1132 <chnandini29@gmail.com>
2020-03-01 22:40:45 -0500

    first commit

--- if.sh
+++ if.sh
@@ -1,7 +1,9 @@
 int1=$1
 int2=$2
 if [ $int1 -eq $int2 ];then
-echo "values are equal "
+echo "values are equal"
+elif [ $int1 -gt $int2 ];then
+echo "values are greater"
 else
-echo "values are not equal"
+echo "values are invalid"
 fi
